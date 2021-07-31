ef1118158ab093b7cf9241c0a93b9165bf50d38c
Rajat Verma <i.rajatverma21@gmail.com>
2021-07-31 15:23:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './global.scss';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -15,17 +16,19 @@ const mountNode = document.querySelector('#app');
 const isProduction = process.env.NODE_ENV === 'production';
 
 ReactDOM.render(
-  <Provider store={store}>
-    {/* add base URL for all locations as app is served from a sub-directory in production */}
-    {isProduction ? (
-      <Router basename="/react-starter-pages">
-        <Routes />
-      </Router>
-    ) : (
-      <Router>
-        <Routes />
-      </Router>
-    )}
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      {/* add base URL for all locations as app is served from a sub-directory in production */}
+      {isProduction ? (
+        <Router basename="/react-starter-pages">
+          <Routes />
+        </Router>
+      ) : (
+        <Router>
+          <Routes />
+        </Router>
+      )}
+    </Provider>
+  </React.StrictMode>,
   mountNode
 );
